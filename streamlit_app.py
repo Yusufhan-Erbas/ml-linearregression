54838be618589823eda0984970caac44eaa4e5d6
Yusufhan ERBAŞ <63731504+Yusufhan-Erbas@users.noreply.github.com>
2024-08-06 07:44:18 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🤖 ML Linear Regression')
 
 st.write('Machine Learning Model based on linear regression model')
+
+df = pd.read_csv("Walmart_Sales.csv")
